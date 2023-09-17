135341e9cfc031158bf999a7647a043b3c8e4a6f
Vitor Mateus <vitor7jan@gmail.com>
2023-09-17 14:57:57 -0300

    chore(.net): added connection to sqlite

--- simple-bank-api/Program.cs
+++ simple-bank-api/Program.cs
@@ -1,3 +1,6 @@
+using Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// db config
+builder.Services.AddDbContext<BankContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 var app = builder.Build();
 
