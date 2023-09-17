5f6b4abd3ec4f3b0afddd450f94deec2da0a4810
Vitor Mateus <vitor7jan@gmail.com>
2023-09-17 18:55:00 -0300

    feat(credit/debit): added context

--- simple-bank-api/Context/BankContext.cs
+++ simple-bank-api/Context/BankContext.cs
@@ -8,4 +8,6 @@ public class BankContext : DbContext
     public BankContext(DbContextOptions<BankContext> options) : base(options) { }
 
     public DbSet<Customer> Customers { get; set; }
+    public DbSet<Credit> Credits { get; set; }
+    public DbSet<Debit> Debits { get; set; }
 }
