46be14cb3e6f8ee86f07b3f67ae3f0abea51f48f
Vitor Mateus <vitor7jan@gmail.com>
2023-09-17 19:57:39 -0300

    feat(transactions): added get balance

--- simple-bank-api/Controllers/TransactionsController.cs
+++ simple-bank-api/Controllers/TransactionsController.cs
@@ -64,4 +64,38 @@ public class TransactionsController : ControllerBase
             return StatusCode(500);
         }
     }
+
+    [HttpGet("balance/{customerId}")]
+    public async Task<IActionResult> Balance(int customerId)
+    {
+        try
+        {
+            Customer? customer = await _context.Customers
+                .FirstOrDefaultAsync(c => c.Active && c.Id == customerId);
+            if (customer == null) return NotFound();
+            double creditsSum = (await GetCreditTransactionsFromCustomer(customer)).Sum(c => c.Value);
+            double debitsSum = -1 * (await GetDebitTransactionsFromCustomer(customer)).Sum(d => d.Value);
+            // sum tranfers
+            double balance = creditsSum + debitsSum;
+            return Ok(new { balance });
+        }
+        catch (Exception)
+        {
+            return StatusCode(500);
+        }
+    }
+
+    private async Task<List<Credit>> GetCreditTransactionsFromCustomer(Customer customer)
+    {
+        var credits = await _context.Credits.AsNoTracking()
+            .Where(c => c.Customer.Equals(customer)).ToListAsync();
+        return credits;
+    }
+
+    private async Task<List<Debit>> GetDebitTransactionsFromCustomer(Customer customer)
+    {
+        var debits = await _context.Debits.AsNoTracking()
+            .Where(d => d.Customer.Equals(customer)).ToListAsync();
+        return debits;
+    }
 }
