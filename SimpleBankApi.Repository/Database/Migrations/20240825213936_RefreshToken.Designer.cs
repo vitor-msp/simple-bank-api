147ca20e49347d49a355b2f33bd69472f18a056f
Vitor Mateus <vitor7jan@gmail.com>
2024-08-25 19:26:07 -0300

    feat(database): added migrations

--- SimpleBankApi.Repository/Database/Migrations/20240825213936_RefreshToken.Designer.cs
+++ SimpleBankApi.Repository/Database/Migrations/20240825213936_RefreshToken.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleBankApi.Repository.Database.Context;
 
@@ -10,9 +11,11 @@ using SimpleBankApi.Repository.Database.Context;
 namespace simple_bank_api.Migrations
 {
     [DbContext(typeof(BankContext))]
-    partial class BankContextModelSnapshot : ModelSnapshot
+    [Migration("20240825213936_RefreshToken")]
+    partial class RefreshToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
---
+++ SimpleBankApi.Repository/Database/Migrations/20240825213936_RefreshToken.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace simple_bank_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiration",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiration",
+                table: "Accounts");
+        }
+    }
+}
