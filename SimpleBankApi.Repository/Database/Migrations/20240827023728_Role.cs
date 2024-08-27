36dd3a66164c2f9fab853718537053be4029ceff
Vitor Mateus <vitor7jan@gmail.com>
2024-08-26 23:49:20 -0300

    feat(account): added role migration

---
+++ SimpleBankApi.Repository/Database/Migrations/20240827023728_Role.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace simple_bank_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Role : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Accounts");
+        }
+    }
+}
