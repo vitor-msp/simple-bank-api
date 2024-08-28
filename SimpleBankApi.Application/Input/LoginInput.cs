c369e2d64e763e6c0d1fd4b68e28cc68bd77973a
Vitor Mateus <vitor7jan@gmail.com>
2024-08-27 21:36:15 -0300

    feat(session): added input attributes for validation

--- SimpleBankApi.Application/Input/LoginInput.cs
+++ SimpleBankApi.Application/Input/LoginInput.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SimpleBankApi.Application.Input;
 
 public class LoginInput
 {
+    [Required]
     public int AccountNumber { get; set; }
+
+    [Required]
     public string Password { get; set; } = "";
 }
--- SimpleBankApi.Application/Input/LogoutInput.cs
+++ SimpleBankApi.Application/Input/LogoutInput.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SimpleBankApi.Application.Input;
 
 public class LogoutInput
 {
+    [Required]
     public int AccountNumber { get; set; }
+
+    [Required]
     public string RefreshToken { get; set; } = "";
 }
