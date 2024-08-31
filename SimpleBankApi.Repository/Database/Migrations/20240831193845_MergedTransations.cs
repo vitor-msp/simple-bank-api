17b072e43cd9c863a04af316f6ed5c4adbd8cce7
Vitor Mateus <vitor7jan@gmail.com>
2024-08-31 16:40:41 -0300

    feat(database): updated migrations

---
+++ SimpleBankApi.Repository/Database/Migrations/20240831193845_MergedTransations.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace simple_bank_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MergedTransations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Customers_OwnerId",
+                table: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Credits");
+
+            migrationBuilder.DropTable(
+                name: "Debits");
+
+            migrationBuilder.DropTable(
+                name: "Transfers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerId",
+                table: "Accounts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Value = table.Column<double>(type: "REAL", nullable: false),
+                    OperatingAccountId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RelatedAccountId = table.Column<int>(type: "INTEGER", nullable: true),
+                    TransactionType = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Accounts_OperatingAccountId",
+                        column: x => x.OperatingAccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Accounts_RelatedAccountId",
+                        column: x => x.RelatedAccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AccountNumber",
+                table: "Accounts",
+                column: "AccountNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_OperatingAccountId",
+                table: "Transactions",
+                column: "OperatingAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_RelatedAccountId",
+                table: "Transactions",
+                column: "RelatedAccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Customers_OwnerId",
+                table: "Accounts",
+                column: "OwnerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Customers_OwnerId",
+                table: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_AccountNumber",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerId",
+                table: "Accounts",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.CreateTable(
+                name: "Credits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AccountId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Value = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Credits_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Debits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AccountId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Value = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Debits", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Debits_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transfers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RecipientId = table.Column<int>(type: "INTEGER", nullable: true),
+                    SenderId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Value = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transfers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transfers_Accounts_RecipientId",
+                        column: x => x.RecipientId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Transfers_Accounts_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Credits_AccountId",
+                table: "Credits",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debits_AccountId",
+                table: "Debits",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transfers_RecipientId",
+                table: "Transfers",
+                column: "RecipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transfers_SenderId",
+                table: "Transfers",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Customers_OwnerId",
+                table: "Accounts",
+                column: "OwnerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+        }
+    }
+}
