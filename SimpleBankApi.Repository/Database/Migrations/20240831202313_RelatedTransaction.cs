20fa81873dd33d6f7e9aa29371287f08dd33c908
Vitor Mateus <vitor7jan@gmail.com>
2024-08-31 17:30:39 -0300

    feat(database): updated migrations

---
+++ SimpleBankApi.Repository/Database/Migrations/20240831202313_RelatedTransaction.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace simple_bank_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelatedTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RelatedTransactionId",
+                table: "Transactions",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_RelatedTransactionId",
+                table: "Transactions",
+                column: "RelatedTransactionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Transactions_RelatedTransactionId",
+                table: "Transactions",
+                column: "RelatedTransactionId",
+                principalTable: "Transactions",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Transactions_RelatedTransactionId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_RelatedTransactionId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "RelatedTransactionId",
+                table: "Transactions");
+        }
+    }
+}
